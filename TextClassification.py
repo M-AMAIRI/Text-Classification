74b68c5aefcac465b3b4ffbccbc3340107523da6
moatez amairi <35851660+M-AMAIRI@users.noreply.github.com>
2019-06-15 08:57:04 +0200

    using random forest

--- TextClassification.py
+++ TextClassification.py
@@ -1,7 +1,12 @@
 import numpy as np  
 import re  
 import nltk  
-from sklearn.datasets import load_files  
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
 nltk.download('stopwords')
 nltk.download('wordnet')
 
@@ -12,16 +17,10 @@ movie_data = load_files("txt_sentoken")
 X, y = movie_data.data, movie_data.target
 
 
-
-#print(y) # [0 1 1 ... 1 0 0] array with 2000 elements
-#print(len(X))
-#We have two categories: "neg" and "pos", therefore 1s and 0s have been added to the target array
+print(y) # listee all files data with labels class
 
 
-
-
-#Once the dataset has been imported, the next step is to preprocess the text. Text may contain numbers, special characters, and unwanted spaces. Depending upon the problem we face, we may or may not need to remove these special characters and numbers from text. However, for the sake of explanation, we will remove all the special characters, numbers, and unwanted spaces from our text. Execute the following script to preprocess the data
-
+# Text Preprocessing :
 
 documents = []
 
@@ -57,124 +56,52 @@ for sen in range(0, len(X)):
     documents.append(document)
 
 
-from sklearn.feature_extraction.text import TfidfVectorizer  
-tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))  
+# Converting Text to Numbers :
+
+tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))  # or tfidfconverter = TfidfTransformer()
 X = tfidfconverter.fit_transform(documents).toarray()
 
-#print(X)
 
-'''
-a small example about TfidfVectorizer
+# Training and Testing Sets :
 
->>> from sklearn.feature_extraction.text import TfidfVectorizer
->>> corpus = [
-...     'This is the first document.',
-...     'This document is the second document.',
-...     'And this is the third one.',
-...     'Is this the first document?',
-... ]
->>> vectorizer = TfidfVectorizer()
->>> X = vectorizer.fit_transform(corpus)
->>> print(vectorizer.get_feature_names())
-['and', 'document', 'first', 'is', 'one', 'second', 'the', 'third', 'this']
->>> print(X.shape)
-(4, 9)
-
-
-'''
-
-
-from sklearn.model_selection import train_test_split  
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) 
-# Split arrays or matrices into random train and test subsets
 
 
-
-#print("len(X_train)")
-#print(len(X_train))
-
-#print("len(X_test)")
-#print(len(X_test))
-
-#print("len(y_train)")
-#print(len(y_train))
-
-#print("len(y_test)")
-#print(len(y_test))
-
-
-'''
-
-an example about train_test_split :
-
->>> import numpy as np
->>> from sklearn.model_selection import train_test_split
->>> X, y = np.arange(10).reshape((5, 2)), range(5)
->>> X
-array([[0, 1],
-       [2, 3],
-       [4, 5],
-       [6, 7],
-       [8, 9]])
->>> list(y)
-[0, 1, 2, 3, 4]
-
-
->>> X_train, X_test, y_train, y_test = train_test_split(
-...     X, y, test_size=0.33, random_state=42)
-...
->>> X_train
-array([[4, 5],
-       [0, 1],
-       [6, 7]])
->>> y_train
-[2, 0, 3]
->>> X_test
-array([[2, 3],
-       [8, 9]])
->>> y_test
-[1, 4]
->>>
->>> train_test_split(y, shuffle=False)
-[[0, 1, 2], [3, 4]]
-
-'''
-
-
-# Training Text Classification Model and Predicting Sentiment
-from sklearn.ensemble import RandomForestClassifier
+# Training Text Classification Model and Predicting Sentiment :
 
 classifier = RandomForestClassifier(n_estimators=1000, random_state=0)  
 classifier.fit(X_train, y_train)
 
-y_pred = classifier.predict(X_test)  
 
-#print(len(y_pred))
+# Evaluating the Model 
 
+y_pred = classifier.predict(X_test) 
 
-# Evaluating the Model
-
-
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-
+print("print(confusion_matrix(y_test,y_pred)) : ")
 print(confusion_matrix(y_test,y_pred))  
-print(classification_report(y_test,y_pred))  
+
+target_names = ['class 0', 'class 1', 'class 2']
+print("print(classification_report(y_test,y_pred)) : ")
+print(classification_report(y_test,y_pred,target_names=target_names))
+
+print("print(accuracy_score(y_test, y_pred)) : ")
 print(accuracy_score(y_test, y_pred))
 
 
-#Saving and Loading the Model
+'''
+Saving and Loading the Model :
+We can save our model as a pickle object in Python. To do so, execute the following script:
+
 with open('text_classifier', 'wb') as picklefile:  
     pickle.dump(classifier,picklefile)
 
 
 
+and then To load the model, we can use the following code:
 
-#To load the model, we can use the following code:
-'''with open('text_classifier', 'rb') as training_model:  
+with open('text_classifier', 'rb') as training_model:  
     model = pickle.load(training_model)
-    y_pred = model.predict(X_test)
+
+y_pred2 = model.predict(X_test)
+
 '''
-
-
-
- 
